5926f9f39c9d56d46fbca7254ab747db0ba40606
Egor Dekhov <dehoff.egor@gmail.com>
2019-10-02 23:07:46 +0300

    Коммит

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
-var dropdownShowBtn = document.querySelector('.navigation__button');
-var sliderControlsBtns = document.querySelectorAll('.slider-controls__button');
+var page = {
+  buttons: {
+    dropdownShow: document.querySelector('.navigation__button'),
+    sliderControls: document.querySelectorAll('.slider-controls__button'),
+    showContactsModal: document.querySelector('.contacts__button'),
+    showMapModal: document.querySelector('.contacts__map')
+  }
+}
+
+function showContactsModal() {
+  var closeModal = document.querySelector('#modal-contacts-close');
+  var contactsModal = document.querySelector('.contacts-modal');
+  closeModal.addEventListener('click', function(){
+      contactsModal.classList.remove('contacts-modal--open');
+  });
+  contactsModal.classList.add('contacts-modal--open');
+}
+
+function showMapModal() {
+  var closeModal = document.querySelector('#modal-map-close');
+  var mapModal = document.querySelector('.map-modal');
+  closeModal.addEventListener('click', function(){
+      mapModal.classList.remove('map-modal--open');
+  });
+  mapModal.classList.add('map-modal--open');
+}
 
 function sliderControlHandler() {
     var slides = document.querySelector('.slider__cards');
 
     return function(e) {
-        if (e.target === sliderControlsBtns[0]) {
+        if (e.target === page.buttons.sliderControls[0]) {
             slides.style.transform = 'translateY(' + 0 + 'px)';
-        } else if (e.target === sliderControlsBtns[1]) {
+        } else if (e.target === page.buttons.sliderControls[1]) {
             slides.style.transform = 'translateY(' + -600 + 'px)';
-        } else if (e.target === sliderControlsBtns[2]) {
+        } else if (e.target === page.buttons.sliderControls[2]) {
             slides.style.transform = 'translateY(' + -1200 + 'px)';
         }
     }
@@ -30,8 +54,12 @@ function dropdownShowHandler() {
     }
 }
 
-dropdownShowBtn.addEventListener('click', dropdownShowHandler());
+page.buttons.dropdownShow.addEventListener('click', dropdownShowHandler());
 
-for (var i = 0; i < sliderControlsBtns.length; i++) {
-    sliderControlsBtns[i].addEventListener('click', sliderControlHandler());
+for (var i = 0; i < page.buttons.sliderControls.length; i++) {
+    page.buttons.sliderControls[i].addEventListener('click', sliderControlHandler());
 }
+
+
+page.buttons.showContactsModal.addEventListener('click', showContactsModal);
+page.buttons.showMapModal.addEventListener('click', showMapModal);
